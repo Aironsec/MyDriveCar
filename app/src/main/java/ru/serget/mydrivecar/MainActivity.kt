e4d6d0d1160543c285845be1b52e57ede0ccc720
SergeyT <airon_man@inbox.ru>
2022-04-18 00:06:06 +0300

    delete function completebleSubject for animator

--- app/src/main/java/ru/serget/mydrivecar/MainActivity.kt
+++ app/src/main/java/ru/serget/mydrivecar/MainActivity.kt
@@ -1,13 +1,9 @@
 package ru.serget.mydrivecar
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import io.reactivex.rxjava3.subjects.CompletableSubject
 import ru.serget.mydrivecar.PaintView.Companion.observerXYList
 import ru.serget.mydrivecar.databinding.ActivityMainBinding
 
@@ -30,19 +26,5 @@ class MainActivity : AppCompatActivity() {
                     taxiView.translationY = it.second
                 }
         }
-    }
-
-    private fun View.gogoTrail(xx: Float, yy: Float, duration: Long = 50L): Completable {
-        val animationSubject = CompletableSubject.create()
-        return animationSubject.doOnSubscribe {
-            ViewCompat.animate(this)
-                .setDuration(duration)
-                .translationX(xx)
-                .translationY(yy)
-                .withEndAction {
-                    animationSubject.onComplete()
-                }
-        }
-
     }
 }
